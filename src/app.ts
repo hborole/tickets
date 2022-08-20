76255379e4aa7d8cf739ebbcf46a4a45e3a226db
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-20 11:01:23 +0530

    Updated common package and create show route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session';
 
 import { NotFoundError, errorHandler, currentUser } from '@hbofficial/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 
 app.use(currentUser);
 
+app.use(showTicketRouter);
 app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
