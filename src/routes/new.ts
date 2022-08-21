ec8ef7039cbf972ba7d3dbdc990fc92a19b1ab6d
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-21 12:34:58 +0530

    Created singleton to connect to nats

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,6 +1,7 @@
 import { requireAuth, validateRequest } from '@hbofficial/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
@@ -21,6 +22,13 @@ router.post(
 
     const ticket = await Ticket.build({ title, price, userId: id });
     await ticket.save();
+
+    // await new TicketCreatedPublisher(client).publish({
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   id: ticket.id,
+    //   userId: ticket.userId,
+    // });
 
     return res.status(201).send(ticket);
   }
