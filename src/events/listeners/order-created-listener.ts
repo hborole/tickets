12e22c24e9de05d0cc476e8e9da5cd2dcb9b8c5f
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-23 13:33:17 +0530

    Order created listener and test

--- src/events/listeners/order-created-listener.ts
+++ src/events/listeners/order-created-listener.ts
@@ -1,10 +1,27 @@
 import { Message } from 'node-nats-streaming';
-import { Listener, OrderCreatedEvent, Subjects } from '@hbofficial/common';
+import { Ticket } from '../../models/ticket';
+import {
+  Listener,
+  NotFoundError,
+  OrderCreatedEvent,
+  Subjects,
+} from '@hbofficial/common';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    ticket.orderId = data.id;
+    await ticket.save();
+
+    msg.ack();
+  }
 }
